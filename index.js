88b8d703017ed344eb37fea252624bc009c98010
Jack Sanders <Guitarbreakerjs@gmail.com>
2021-02-11 19:26:49 -0700

    basic email script for lambda

---
+++ index.js
@@ -0,0 +1,31 @@
+exports.handler = async (event, context) => {
+  let statusCode = "200";
+    
+  if (event.httpMethod === "POST"){
+    const sgMail = require("@sendgrid/mail");
+    sgMail.setApiKey(process.env.SECRET_KEY)
+
+    const { email, message } = event.body
+
+    try {
+      await sgMail.send({
+        to: "jack.sanders.xyz@gmail.com",
+        from: "jack.sanders.xyz@gmail.com",
+        subject: "Inbound message from jacksanders.xyz",
+        text: "message",
+        html: "<strong>email</>",
+      })
+    } catch (err) {
+      statusCode = "500";
+    }
+  }
+
+  return {
+    statusCode,
+    headers: {
+      "Access-Control-Allow-Origin": "https://jacksanders.xyz",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Methods": "OPTIONS,POST"
+    },
+  };
+};
